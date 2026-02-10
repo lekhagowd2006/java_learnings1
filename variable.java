ef22f3be8d4d7e9f680f3629a7b6a40790d75b88
Lekha kc <lekhagowda2006@gmail.com>
2026-02-10 13:46:11 +0530

    completed till user input

---
+++ variable.java
@@ -0,0 +1,21 @@
+class variable{
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "3";
+        float c = 10.5f;
+        double d = 10.5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+
+class variable2{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println("a+b");
+    }
+}
