26282cd6db9434820ec6a53e6dccc17d731032c6
Lekha kc <lekhagowda2006@gmail.com>
2026-02-11 14:13:22 +0530

    completed

---
+++ basic_patterns.java
@@ -0,0 +1,25 @@
+class RectanglePattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class InvertedTriangle {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 1; i++) {
+            for (int j = 1; j <= 1; j++) {
+                System.out.println("* ");
+            }
+            System.out.print(" ")
+        }
+    }
+}
+
+class HollowSquare {
+    public
+}
---
+++ loop_examples.java
@@ -0,0 +1,16 @@
+class forLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class forLoopCountDown {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
